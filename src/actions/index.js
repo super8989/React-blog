eb92c9048c963651670ee38a0f2e1833c0878a90
Sam <super8989@gmail.com>
2019-03-17 16:09:20 -0400

    refactor on action creator with .chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,27 @@
-import _ from 'lodash';
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-
   await dispatch(fetchPosts());
 
-
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
   userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 //defining a function that is returning a function
 //returns an array
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get("/posts");
+  const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
 
 //returns a single object for that user
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
-
 
 //returns a single object for that user
 // export const fetchUser = id => dispatch => {

5b8ab221be26da7bb8f71f7757bf04e22e5c5044
Sam <super8989@gmail.com>
2019-03-17 15:42:23 -0400

    alternate overfetching solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,12 +10,20 @@ export const fetchPosts = () => async dispatch => {
   };
 
 //returns a single object for that user
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+
+//returns a single object for that user
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });

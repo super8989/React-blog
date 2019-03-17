6bdfbc518532d77549e9941ee1cd1e60bc67faa9
Sam <super8989@gmail.com>
2019-03-17 13:21:54 -0400

    switch statement in reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,8 @@
 export default (state = [], action) => {
-  if (action.type === 'FETCH_POSTS') {
-    return action.payload;
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
   }
-
-  return state;
 };

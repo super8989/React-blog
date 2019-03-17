d58686b96e7256850603c1c74ab1f4bc56691be8
Sam <super8989@gmail.com>
2019-03-17 13:57:58 -0400

    fetching singular records

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+//defining a function that is returning a function
+//returns an array
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/posts");
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
   };
 
-//defining a function that is returning a function
+//returns a single object for that user
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+}

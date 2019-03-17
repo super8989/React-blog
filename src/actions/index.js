d026ad2862e1e0df55c75bffa8912821416047d7
Sam <super8989@gmail.com>
2019-03-17 15:50:44 -0400

    action creators in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('about to fetch posts');
+  await dispatch(fetchPosts());
+  console.log('fetched posts');
+};
+
 //defining a function that is returning a function
 //returns an array
 export const fetchPosts = () => async dispatch => {
